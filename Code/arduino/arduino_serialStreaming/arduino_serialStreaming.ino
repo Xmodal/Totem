d0af1391fdfe073adbc8ba8d4ecf7f2790df924b
alx <saunier.alexandre@yahoo.com>
2019-04-17 16:37:14 -0400

    updated arduino code

--- Code/arduino/arduino_serialStreaming/arduino_serialStreaming.ino
+++ Code/arduino/arduino_serialStreaming/arduino_serialStreaming.ino
@@ -1,13 +1,12 @@
 #include <FastLED.h>
 
-#define COLS 360 // 36 leds * 11 boxes
-#define ROWS 9
-#define NUM_LEDS 360 // 36 leds * 11 boxes, number of leds per output (9 per pcb)
+#define ROWS 360 // 36 leds * 10 boxes
+#define COLS 9
 #define RATE 3 // 3 seems like the ideal value, slowdown if glitches appear at the end of the strips but watchout for dropping FPS
 
 //set teensy at 144 mhz
 
-CRGB leds[9][NUM_LEDS];//holds the values of the leds for fastled output
+CRGB leds[COLS][ROWS];//holds the values of the leds for fastled output
 
 int indexcount;//counter for serial, resets on incoming byte 255, then counts which led the serial is receiving
 
@@ -18,21 +17,21 @@ void setup() {
   Serial.begin(2500000);// baud rate 2500000
 
   //setup led outputs, port numbers and order tested, from the pcb schematic
-  FastLED.addLeds<APA102, 5, 6, BGR, DATA_RATE_MHZ(RATE)>(leds[2], NUM_LEDS);
-  FastLED.addLeds<APA102, 7, 8, BGR, DATA_RATE_MHZ(RATE)>(leds[1], NUM_LEDS);
-  FastLED.addLeds<APA102, 25, 26, BGR, DATA_RATE_MHZ(RATE)>(leds[0], NUM_LEDS);
-  FastLED.addLeds<APA102, 27, 28, BGR, DATA_RATE_MHZ(RATE)>(leds[5], NUM_LEDS);
-  FastLED.addLeds<APA102, 29, 30, BGR, DATA_RATE_MHZ(RATE)>(leds[4], NUM_LEDS);
-  FastLED.addLeds<APA102, 31, 32, BGR, DATA_RATE_MHZ(RATE)>(leds[3], NUM_LEDS);
-  FastLED.addLeds<APA102, 0, 1, BGR, DATA_RATE_MHZ(RATE)>(leds[8], NUM_LEDS);
-  FastLED.addLeds<APA102, 3, 4, BGR, DATA_RATE_MHZ(RATE)>(leds[7], NUM_LEDS);
-  FastLED.addLeds<APA102, 19, 18, BGR, DATA_RATE_MHZ(RATE)>(leds[6], NUM_LEDS);
+  FastLED.addLeds<APA102, 5, 6, BGR, DATA_RATE_MHZ(RATE)>(leds[2], ROWS);
+  FastLED.addLeds<APA102, 7, 8, BGR, DATA_RATE_MHZ(RATE)>(leds[1], ROWS);
+  FastLED.addLeds<APA102, 25, 26, BGR, DATA_RATE_MHZ(RATE)>(leds[0], ROWS);
+  FastLED.addLeds<APA102, 27, 28, BGR, DATA_RATE_MHZ(RATE)>(leds[5], ROWS);
+  FastLED.addLeds<APA102, 29, 30, BGR, DATA_RATE_MHZ(RATE)>(leds[4], ROWS);
+  FastLED.addLeds<APA102, 31, 32, BGR, DATA_RATE_MHZ(RATE)>(leds[3], ROWS);
+  FastLED.addLeds<APA102, 0, 1, BGR, DATA_RATE_MHZ(RATE)>(leds[8], ROWS);
+  FastLED.addLeds<APA102, 3, 4, BGR, DATA_RATE_MHZ(RATE)>(leds[7], ROWS);
+  FastLED.addLeds<APA102, 19, 18, BGR, DATA_RATE_MHZ(RATE)>(leds[6], ROWS);
 }
 
 void loop() {
-  
+
   //the following sets a single led's value and the counter
-  while (Serial.available() > 0) {  
+  while (Serial.available() > 0) {
     int incomingByte = Serial.read();//read single byte
     if (incomingByte == 255)//reset byte is 255, resets the counter for setting led values
     {
@@ -43,8 +42,8 @@ void loop() {
       int value = incomingByte;
       if (indexcount < ROWS * COLS)//check if counter hasn't glitched
       {
-        int x = floor(float(indexcount) / COLS);//get strip output index [0 -> 9] from counter
-        int y = indexcount % COLS;//get led index in strip [0 -> 396] from counter
+        int x = indexcount%COLS;
+        int y = indexcount/COLS; //get strip output index [0 -> 9] from counter
         //set led values
         leds[x][y].red = value;
         leds[x][y].green = value;
@@ -62,6 +61,6 @@ void loop() {
     FastLED.show();//send led data to strips
     lastOutputTime = millis();//update time for output counter
   }
-  
+
 }
 
